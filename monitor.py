e04228a1c249d49c7683450ae1808426a02c311c
James Warren <jwarren116@gmail.com>
2015-08-04 16:46:55 -0700

    - Import sites to monitor from external text file - Add SMTP notification support - Import SMTP settings from external Python file - NOTE: Currently only actually monitoring first site in site file, other sites are not actually monitored yet

--- monitor.py
+++ monitor.py
@@ -1,15 +1,30 @@
 #!/usr/bin/env python
-import requests
+from __future__ import unicode_literals
 from time import sleep
+import requests
+import io
+import smtplib
+from smtp_config import sender, password, receivers, host, port, message
 
-SITE = "http://www.jwarren.co"
+SITES = io.open('sites.txt', mode='r').readlines()
+SITES = [site.strip() for site in SITES]
 
-print "Beginning monitoring of {}".format(SITE)
+for SITE in SITES:
+    print "Beginning monitoring of {}".format(SITE)
 
 try:
     while True:
-        resp = requests.get(SITE)
-        print "Status: {}".format(resp.status_code)
-        sleep(5)
+        resp = requests.get(SITES[0])
+        if resp.status_code != 200:
+            print "{} status: {}".format(SITES[0], resp.status_code)
+            try:
+                smtpObj = smtplib.SMTP(host, port)
+                smtpObj.starttls()
+                smtpObj.login(sender, password)
+                smtpObj.sendmail(sender, receivers, message)
+                print "Successfully sent email"
+            except smtplib.SMTPException:
+                print "Error sending email ({}:{})".format(host, port)
+        sleep(3)
 except KeyboardInterrupt:
-    print " -- Monitoring canceled -- "
+    print "\n-- Monitoring canceled --"

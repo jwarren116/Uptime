6dfb1c1b1d30aa2d7d6c9872a46671ef6b6d57a5
James Warren <jwarren116@gmail.com>
2015-08-04 17:52:51 -0700

    Change script to only send an alert email if one has not been sent for a certain defined amount of time

--- monitor.py
+++ monitor.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from __future__ import unicode_literals
-from time import sleep
+from time import sleep, time
 import requests
 import io
 import smtplib
@@ -8,6 +8,11 @@ from smtp_config import sender, password, receivers, host, port, message
 
 SITES = io.open('sites.txt', mode='r').readlines()
 SITES = [site.strip() for site in SITES]
+
+DELAY = 60 # Delay between site queries
+EMAIL_INTERVAL = 1800 # Delay between alert emails
+
+time_email_sent = 0
 
 for SITE in SITES:
     print "Beginning monitoring of {}".format(SITE)
@@ -17,14 +22,16 @@ try:
         resp = requests.get(SITES[0])
         if resp.status_code != 200:
             print "{} status: {}".format(SITES[0], resp.status_code)
-            try:
-                smtpObj = smtplib.SMTP(host, port)
-                smtpObj.starttls()
-                smtpObj.login(sender, password)
-                smtpObj.sendmail(sender, receivers, message)
-                print "Successfully sent email"
-            except smtplib.SMTPException:
-                print "Error sending email ({}:{})".format(host, port)
-        sleep(3)
+            if (time() - time_email_sent) > EMAIL_INTERVAL:
+                try:
+                    smtpObj = smtplib.SMTP(host, port)
+                    smtpObj.starttls()
+                    smtpObj.login(sender, password)
+                    smtpObj.sendmail(sender, receivers, message)
+                    time_sent = time()
+                    print "Successfully sent email"
+                except smtplib.SMTPException:
+                    print "Error sending email ({}:{})".format(host, port)
+        sleep(DELAY)
 except KeyboardInterrupt:
     print "\n-- Monitoring canceled --"

0426f3182f986c9331b2eeeff11f44e754bf48a0
James Warren <jwarren116@gmail.com>
2015-08-06 11:01:16 -0700

    - Shift script management to __name__=='__main__' block instead of utilizing Threading     - Main block iterates through list of sites and calls monitor() on each site, then sleeps for specified duration     - Main block hands off a dictionary of email timestamps to the monitor() function for reference - Email timestamps are now stored and updated in a dictionary so that emails can be sent for multiple sites in a short period of time - Emails are retried the next iteration if an SMTPException is raised (timestamp is not updated until after sendmail() is successful)

--- monitor.py
+++ monitor.py
@@ -6,36 +6,42 @@ import io
 import smtplib
 from smtp_config import sender, password, receivers, host, port, message
 
-# Read in sites to monitor
-SITES = [site.strip() for site in io.open('sites.txt', mode='r').readlines()]
 
-# Temporarily use single SITE until threading is set up
-SITE = SITES[0]
+DELAY = 60  # Delay between site queries
+EMAIL_INTERVAL = 1800  # Delay between alert emails
 
-DELAY = 60 # Delay between site queries
-EMAIL_INTERVAL = 1800 # Delay between alert emails
 
-time_email_sent = 0
+def monitor(SITE, email_time):
+    resp = requests.get(SITE)
+    if resp.status_code != 200:
+        print "{} status: {}".format(SITE, resp.status_code)
+        # If more than EMAIL_INTERVAL seconds since last email, resend
+        if (time() - email_time[SITE]) > EMAIL_INTERVAL:
+            try:
+                smtpObj = smtplib.SMTP(host, port) # Set up SMTP object
+                smtpObj.starttls()
+                smtpObj.login(sender, password)
+                smtpObj.sendmail(sender, receivers, message)
+                email_time[SITE] = time()
+                print "Successfully sent email"
+            except smtplib.SMTPException:
+                print "Error sending email ({}:{})".format(host, port)
 
-# for SITE in SITES:
-print "Beginning monitoring of {}".format(SITE)
 
-try:
+if __name__ == '__main__':
+    # Read in sites to monitor
+    SITES = [site.strip() for site in io.open('sites.txt', mode='r').readlines()]
+    email_time = {}
+
+    for SITE in SITES:
+        print "Beginning monitoring of {}".format(SITE)
+        email_time[SITE] = 0
+
     while True:
-        resp = requests.get(SITE)
-        if resp.status_code != 200:
-            print "{} status: {}".format(SITE, resp.status_code)
-            # If more than EMAIL_INTERVAL seconds since last email, resend
-            if (time() - time_email_sent) > EMAIL_INTERVAL:
-                try:
-                    smtpObj = smtplib.SMTP(host, port) # Set up SMTP object
-                    smtpObj.starttls()
-                    smtpObj.login(sender, password)
-                    smtpObj.sendmail(sender, receivers, message)
-                    time_sent = time()
-                    print "Successfully sent email"
-                except smtplib.SMTPException:
-                    print "Error sending email ({}:{})".format(host, port)
-        sleep(DELAY)
-except KeyboardInterrupt:
-    print "\n-- Monitoring canceled --"
+        try:
+            for SITE in SITES:
+                monitor(SITE, email_time)
+            sleep(DELAY)
+        except KeyboardInterrupt:
+            print "\n-- Monitoring canceled --"
+            break

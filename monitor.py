eb21e76ca95eb6af0ffc7525622ec3088a6a4c3b
James Warren <jwarren116@gmail.com>
2015-08-14 11:49:47 -0700

    - Split ping() into ping() and error_log() functions that are more specialized and focused - Dictionary last_email_time is defined at the global level so it doesn't need to be passed to ping for every call

--- monitor.py
+++ monitor.py
@@ -11,6 +11,8 @@ from smtp_config import sender, password, receivers, host, port
 DELAY = 60  # Delay between site queries
 EMAIL_INTERVAL = 1800  # Delay between alert emails
 
+last_email_time = {}  # Monitored sites and timestamp of last alert sent
+
 # Define escape sequences for colored terminal output
 COLOR_DICT = {
     "green": "\033[92m",
@@ -34,42 +36,46 @@ def colorize(text, color):
     return COLOR_DICT[color] + str(text) + COLOR_DICT['end']
 
 
-def ping(site, last_email_time):
+def error_log(site, status_code):
+    # Print colored status message to terminal
+    print "\n({}) {} STATUS: {}".format(strftime("%a %b %d %Y %H:%M:%S"),
+                                        site,
+                                        colorize(status_code, "yellow"),
+                                        )
+    # Log status message to log file
+    with open('monitor.log', 'a') as log:
+        log.write("({}) {} STATUS: {}\n".format(strftime("%a %b %d %Y %H:%M:%S"),
+                                                site,
+                                                status_code,
+                                                )
+                  )
+    # If more than EMAIL_INTERVAL seconds since last email, resend
+    if (time() - last_email_time[site]) > EMAIL_INTERVAL:
+        try:
+            smtpObj = smtplib.SMTP(host, port)  # Set up SMTP object
+            smtpObj.starttls()
+            smtpObj.login(sender, password)
+            smtpObj.sendmail(sender,
+                             receivers,
+                             MESSAGE.format(sender=sender,
+                                            receivers=", ".join(receivers),
+                                            site=site,
+                                            status=status_code
+                                            )
+                             )
+            last_email_time[site] = time()  # Update time of last email
+            print colorize("Successfully sent email", "green")
+        except smtplib.SMTPException:
+            print colorize("Error sending email ({}:{})".format(host, port), "red")
+
+
+def ping(site):
     resp = requests.get(site)
     if resp.status_code == 200:
         print "\b" + colorize(".", "green"),
         sys.stdout.flush()
     else:
-        # Print colored status message to terminal
-        print "\n({}) {} STATUS: {}".format(strftime("%a %b %d %Y %H:%M:%S"),
-                                            site,
-                                            colorize(resp.status_code, "yellow"),
-                                            )
-        # Log status message to log file
-        with open('monitor.log', 'a') as log:
-            log.write("({}) {} STATUS: {}\n".format(strftime("%a %b %d %Y %H:%M:%S"),
-                                                    site,
-                                                    resp.status_code,
-                                                    )
-                      )
-        # If more than EMAIL_INTERVAL seconds since last email, resend
-        if (time() - last_email_time[site]) > EMAIL_INTERVAL:
-            try:
-                smtpObj = smtplib.SMTP(host, port)  # Set up SMTP object
-                smtpObj.starttls()
-                smtpObj.login(sender, password)
-                smtpObj.sendmail(sender,
-                                 receivers,
-                                 MESSAGE.format(sender=sender,
-                                                receivers=", ".join(receivers),
-                                                site=site,
-                                                status=resp.status_code
-                                                )
-                                 )
-                last_email_time[site] = time()  # Update time of last email
-                print colorize("Successfully sent email", "green")
-            except smtplib.SMTPException:
-                print colorize("Error sending email ({}:{})".format(host, port), "red")
+        error_log(site, resp.status_code)
 
 
 def get_sites():
@@ -94,8 +100,6 @@ def get_sites():
 
 
 def main():
-    last_email_time = {}  # Monitored sites and timestamp of last alert sent
-
     sites = get_sites()
 
     for site in sites:
@@ -105,7 +109,7 @@ def main():
     while sites:
         try:
             for site in sites:
-                ping(site, last_email_time)
+                ping(site)
             sleep(DELAY)
         except KeyboardInterrupt:
             print colorize("\n-- Monitoring canceled --", "red")

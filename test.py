13f704e1ad12593fb238945c49c2e1efc0597334
James Warren <jwarren116@gmail.com>
2015-08-17 12:23:37 -0700

    Add test for error_log function; shorten get_sites test; add import for io and monitor.last_email_time

--- test.py
+++ test.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python
-from monitor import main, get_sites, ping, error_log, colorize
+from monitor import main, get_sites, ping, error_log, colorize, last_email_time
 import pytest
 import sys
+import io
 
 
 def test_get_sites():
     """get_sites() function adds URLs from sys.argv"""
     sys.argv.append("www.google.com")
-    sites = get_sites()
-    assert "http://www.google.com" in sites
+    assert "http://www.google.com" in get_sites()
 
 
 def test_colorize():
     """colorize() returns string wrapped in ANSI color tags"""
     assert colorize("text", "red") == "\033[91mtext\033[0m"
+
+
+def test_error_log(capsys):
+    last_email_time["http://www.example.com"] = 0
+    error_log("http://www.example.com", 404)
+    log = [site.strip() for site in io.open('monitor.log', mode='r').readlines()]
+    out, err = capsys.readouterr()
+    assert "http://www.example.com STATUS: \033[93m404" in out
+    assert "http://www.example.com STATUS: 404" in " ".join(log)

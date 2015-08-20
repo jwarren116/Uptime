7a90c8e6466773736a3b6c1799c005f5bb9d78b1
James Warren <jwarren116@gmail.com>
2015-08-19 21:30:23 -0700

    Update tests to reflect new return of ping function

--- test.py
+++ test.py
@@ -25,16 +25,11 @@ def test_error_log(capsys):
     assert "http://www.example.com STATUS: 404" in " ".join(log)
 
 
-def test_ping_valid(capsys):
-    ping("http://www.google.com")
-    out, err = capsys.readouterr()
-    assert "\033[92m." in out
+def test_ping_valid():
+    status = ping("http://www.google.com")
+    assert status == 200
 
 
 def test_ping_invalid(capsys):
-    last_email_time["http://www.jwarren.co/somefakesite"] = 0
-    ping("http://www.jwarren.co/somefakesite")
-    log = [site.strip() for site in io.open('monitor.log', mode='r').readlines()]
-    out, err = capsys.readouterr()
-    assert "http://www.jwarren.co/somefakesite STATUS: \033[93m404" in out
-    assert "http://www.jwarren.co/somefakesite STATUS: 404" in " ".join(log)
+    status = ping("http://www.jwarren.co/somefakesite")
+    assert status == 404

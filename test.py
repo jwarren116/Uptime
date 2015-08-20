2c046918cf88b73be3c6cb39326c04f4591382cd
James Warren <jwarren116@gmail.com>
2015-08-19 21:20:41 -0700

    Add tests for valid and invalid pings

--- test.py
+++ test.py
@@ -23,3 +23,18 @@ def test_error_log(capsys):
     out, err = capsys.readouterr()
     assert "http://www.example.com STATUS: \033[93m404" in out
     assert "http://www.example.com STATUS: 404" in " ".join(log)
+
+
+def test_ping_valid(capsys):
+    ping("http://www.google.com")
+    out, err = capsys.readouterr()
+    assert "\033[92m." in out
+
+
+def test_ping_invalid(capsys):
+    last_email_time["http://www.jwarren.co/somefakesite"] = 0
+    ping("http://www.jwarren.co/somefakesite")
+    log = [site.strip() for site in io.open('monitor.log', mode='r').readlines()]
+    out, err = capsys.readouterr()
+    assert "http://www.jwarren.co/somefakesite STATUS: \033[93m404" in out
+    assert "http://www.jwarren.co/somefakesite STATUS: 404" in " ".join(log)
